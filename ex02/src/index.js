d66e15116f01b0c2115e4f3eb0a0a162e7a538e4
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-25 22:07:48 +0100

    ex02 Delegating to another  generator

---
+++ ex02/src/index.js
@@ -0,0 +1,39 @@
+//Only change code below this line
+let fifteenArray = [];
+function* myGenerator() {
+  yield* insideGenerator1();
+  yield* insideGenerator2();
+  yield* insideGenerator3();
+}
+function* insideGenerator1() {
+  let x;
+  for (let i = 1; i <= 5; i++) {
+    yield (x = i);
+  }
+}
+function* insideGenerator2() {
+  let x;
+  for (let i = 10; i <= 15; i++) {
+    yield (x = i);
+  }
+}
+function* insideGenerator3() {
+  let x;
+  for (let i = 6; i <= 9; i++) {
+    yield (x = i);
+  }
+}
+var iterator = myGenerator();
+for (let i = 0; i < 16; i++) {
+  if (i != 15) {
+    var val = iterator.next().value + "#";
+    fifteenArray.push(val);
+  } else {
+    var v = iterator.next().value + "!";
+    fifteenArray.push(v);
+  }
+}
+console.log(fifteenArray);
+
+//Only change code above this line
+module.exports = {fifteenArray, myGenerator};
