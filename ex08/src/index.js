ff79e40ca771b7c6173742bbbfebf5b9bc1931b8
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-25 23:07:19 +0100

    Mapping and filtering

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+//Only change code below this line
+let one = new Set([1, 2, 3, 4, 5]);
+let two = new Set([6, 4, 3, 2]);
+let unionSet = new Set([...one, ...two]);
+//unionSet.add(...two);
+let intersectionSet = new Set([...one].filter((i) => two.has(i)));
+let differenceSet = new Set([...one].filter((i) => !two.has(i)));
+console.log(one);
+console.log(two);
+console.log(unionSet);
+console.log(intersectionSet);
+console.log(differenceSet);
+
+//Only change code above this line
+module.exports = { unionSet, intersectionSet, differenceSet};
